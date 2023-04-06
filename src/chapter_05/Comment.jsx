c85ce7c9c37a7f35f0a6f4cd3b0ac6c984b2a696
user name <foo@email.com>
2023-04-06 10:40:30 +0900

    ㄴㅇㅁㅇㄴㅇㅁㅇㄴㅁㄴㅇㅁ

---
+++ src/chapter_05/Comment.jsx
@@ -0,0 +1,65 @@
+/*import React from "react";
+
+function Comment(props){
+    return(
+        <div>
+            <h1>내가 만든 첫 콤포논트.</h1>
+        </div>
+    )
+}
+
+export default Comment
+*/
+import React from "react";
+
+const styles = {
+    wrapper: {
+        margin: 8,
+        padding: 8,
+        display: "flex",
+        flexDirection: "row",
+        border: "1px solid grey",
+        borderRadius: 16,
+    },
+    imageContainer: {},
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+    contentContainer: {
+        marginLeft: 8,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+    },
+    nameText: {
+        color: "black",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    commentText: {
+        color: "black",
+        fontSize: 16,
+    },
+};
+
+function Comment(props) {
+    return (
+        <div style={styles.wrapper}>
+            <div style={styles.imageContainer}>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+                    style={styles.image}
+                />
+            </div>
+
+            <div style={styles.contentContainer}>
+                <span style={styles.nameText}>{props.name}</span>
+                <span style={styles.commentText}>{props.comment}</span>
+            </div>
+        </div>
+    );
+}
+
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Clock from './chapter_04/Clock';
+import Comment from './chapter_05/Comment';
+import CommentList from './chapter_05/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Clock />
+    <CommentList/>
   </React.StrictMode>
 );
 
